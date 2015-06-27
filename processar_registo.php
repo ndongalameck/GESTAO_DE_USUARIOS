8a9cca2d2fe169744890f5906b55250aee8e2bd1
ndongalameck <aristofanes.net@gmail.com>
2015-06-27 06:35:16 +0100

    tirando a senha do usuario root

--- processar_registo.php
+++ processar_registo.php
@@ -5,7 +5,7 @@
 		exit;
 	}
 	// ligando a base de dados
-	$ligacao = mysql_connect('localhost', 'root', '123root') or die ('não foi possivel ligar a base de dados');
+	$ligacao = mysql_connect('localhost', 'root', '') or die ('não foi possivel ligar a base de dados');
 	//selecionar a bd pretendida
 	mysql_select_db('gestao_usuarios', $ligacao) or (mysql_error($ligacao));
 	//artibuir as variaveis aos dados recolhidos do form
--- ver_usuario.php
+++ ver_usuario.php
@@ -1,6 +1,6 @@
 <?php
 	//ligar a base de dados
-	$ligacao = mysql_connect('localhost', 'root' '123root') or die('erro ao ligar a base de dados');
+	$ligacao = mysql_connect('localhost', 'root' '') or die('erro ao ligar a base de dados');
 	//ativar a base de dados
 	mysql_select_db('gestao_usuarios' $ligacao) or die (mysql_error($ligacao));
 	//criar a consulta a base de dados
