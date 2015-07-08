941109516ff9266e56ce11b4dc9dd1ffde4368ba
ndongalameck <aristofanes.net@gmail.com>
2015-07-08 12:30:38 +0100

    deleting the passwd

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 	// ligando a base de dados
-		$ligacao = mysql_connect('localhost', 'root', '123root') or die ('não foi possivel ligar a base de dados');
+		$ligacao = mysql_connect('localhost', 'root', '') or die ('não foi possivel ligar a base de dados');
 		//selecionar a bd pretendida
 		mysql_select_db('gestao_usuarios', $ligacao) or (mysql_error($ligacao));
 ?>
